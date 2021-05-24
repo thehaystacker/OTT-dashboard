ed427b707fccc46e1bcb9a89358c143c820643c2
thehaystacker <thehaystacker.in@gmail.com>
2021-05-24 22:04:38 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const app = express();
+const path = require("path");
 
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
+const app = express();
+app.use(express.static(path.resolve(__dirname, "client/build")));
+
+app.get("/api", (req, res) => {
 	res.send({ message: "success" });
+});
+
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
 });
 
 app.listen(port, () => {
