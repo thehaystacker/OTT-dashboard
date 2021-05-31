1f55505d88bfac8bd8388bc2c2ed9dc9ad861b4d
thehaystacker <thehaystacker.in@gmail.com>
2021-05-31 20:14:42 +0530

    Route change

--- server/index.js
+++ server/index.js
@@ -5,17 +5,15 @@ const port = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 app.get("/api", (req, res) => {
 	res.send({ message: "success" });
 });
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-	});
-}
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 
 app.listen(port, () => {
 	console.log("App is listening on port " + port);
