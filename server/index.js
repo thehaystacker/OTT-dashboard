d047f780604a0ef68efdcbe6f1ae7d6adc1a2d71
thehaystacker <thehaystacker.in@gmail.com>
2021-05-31 20:06:11 +0530

    Fetch added to client

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
-var express = require("express");
-var path = require("path");
-var port = process.env.PORT || 3001;
-var app = express();
+const express = require("express");
+const path = require("path");
+
+const port = process.env.PORT || 3001;
+
+const app = express();
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, "../client/build")));
-    app.get("*", function (req, res) {
-        res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-    });
+	app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+	});
 }
-app.get("/api", function (req, res) {
-    res.send({ message: "success" });
+
+app.get("/api", (req, res) => {
+	res.send({ message: "success" });
 });
-app.listen(port, function () {
-    console.log("App is listening on port " + port);
+
+app.listen(port, () => {
+	console.log("App is listening on port " + port);
 });
