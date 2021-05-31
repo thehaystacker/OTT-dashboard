2adb46841a339fb5a3e292a039fa3bff3e7ecc24
thehaystacker <thehaystacker.in@gmail.com>
2021-05-31 20:11:48 +0530

    Route change

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const path = require("path");
 const port = process.env.PORT || 3001;
 
 const app = express();
+
+app.get("/api", (req, res) => {
+	res.send({ message: "success" });
+});
+
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.resolve(__dirname, "../client/build")));
 
@@ -11,10 +16,6 @@ if (process.env.NODE_ENV === "production") {
 		res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 	});
 }
-
-app.get("/api", (req, res) => {
-	res.send({ message: "success" });
-});
 
 app.listen(port, () => {
 	console.log("App is listening on port " + port);
