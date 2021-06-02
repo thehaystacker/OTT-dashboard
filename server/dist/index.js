a4cab544c3bae9931f3abe033c1e95a4aa5f088d
thehaystacker <thehaystacker.in@gmail.com>
2021-06-02 19:27:15 +0530

    test - heroku build

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,16 +1,19 @@
 "use strict";
-var __importDefault =
-	(this && this.__importDefault) ||
-	function (mod) {
-		return mod && mod.__esModule ? mod : { default: mod };
-	};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var path_1 = __importDefault(require("path"));
 var app = express_1.default();
 var port = process.env.PORT || 3001;
+app.use(express_1.default.static(path_1.default.resolve(__dirname, "../../client/build")));
 app.get("/", function (req, res) {
-	res.send({ message: "Hello" });
+    res.send({ message: "Hello" });
+});
+app.get("*", function (req, res) {
+    res.sendFile(path_1.default.resolve(__dirname, "../../client/build", "index.html"));
 });
 app.listen(port, function () {
-	console.log("server is listening on " + port);
+    console.log("server is listening on " + port);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,14 @@ import path from "path";
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.static(path.resolve(__dirname, "../../client/build")));
+
 app.get("/", (req, res) => {
 	res.send({ message: "Hello" });
+});
+
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../../client/build", "index.html"));
 });
 
 app.listen(port, () => {
