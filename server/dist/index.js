619112d41186decfcdcbfdda9b754564c17d9cf5
thehaystacker <thehaystacker.in@gmail.com>
2021-06-02 19:09:27 +0530

    test - heroku build

--- server/dist/index.js
+++ server/dist/index.js
@@ -6,13 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
 var port = 3001;
-// app.use(express.static(path.resolve(__dirname, "../../client/build")));
-app.get("/api", function (req, res) {
-    res.send("The sedulous hyena ate");
+app.get("/", function (req, res) {
+    res.send({ message: "Hello" });
 });
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, "../../client/build", "index.html"));
-// });
 app.listen(port, function () {
     console.log("server is listening on " + port);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,15 +4,9 @@ import path from "path";
 const app = express();
 const port = 3001;
 
-// app.use(express.static(path.resolve(__dirname, "../../client/build")));
-
-app.get("/api", (req, res) => {
-	res.send("The sedulous hyena ate");
+app.get("/", (req, res) => {
+	res.send({ message: "Hello" });
 });
-
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, "../../client/build", "index.html"));
-// });
 
 app.listen(port, () => {
 	console.log(`server is listening on ${port}`);
