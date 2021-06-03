05869e881c5e53ab034c64c8ff372c2ba6b37b80
thehaystacker <thehaystacker.in@gmail.com>
2021-06-03 19:29:04 +0530

    Adding code to github

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,19 @@
-import express from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import path from "path";
 
-const app = express();
+const app: Application = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.static(path.resolve(__dirname, "../../client/build")));
 
-app.get("/", (req, res) => {
+app.get("/api", (req: Request, res: Response) => {
 	res.send({ message: "Hello" });
 });
 
-app.get("*", (req, res) => {
+app.get("*", (req: Request, res: Response) => {
 	res.sendFile(path.resolve(__dirname, "../../client/build", "index.html"));
 });
 
-app.listen(port, () => {
+app.listen(port as number, (): void => {
 	console.log(`server is listening on ${port}`);
 });
